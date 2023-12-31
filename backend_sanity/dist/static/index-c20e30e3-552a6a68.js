29abd9b579d5e1f17cf5078ffe8040814f91f824
Kuldeep Saini <89005887+kuldeepsaini23@users.noreply.github.com>
2023-12-31 21:57:59 +0530

    Add files via upload

---
+++ backend_sanity/dist/static/index-c20e30e3-552a6a68.js
@@ -0,0 +1,1 @@
+import{DocumentPaneProvider as m,DocumentPane as a}from"./index-63f13ccc-88b7b520.js";import"./sanity-1980e088.js";import"./index-17a5f9e3.js";export{m as DocumentPaneProvider,a as default};
